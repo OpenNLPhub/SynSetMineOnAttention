d04b8691a279d237853b78767c96e0f2c3024d92
Zreal <zenglz_pro@163.com>
2020-11-29 14:39:17 +0800

    add model based on attentionn

--- model.py
+++ model.py
@@ -5,14 +5,13 @@
  * @desc 
 '''
 
-from typing import List
+from typing import Dict, List
 import torch
-from torch._C import set_flush_denormal
 import torch.nn as nn
-from torch.nn.modules import dropout
 from transformers import BertModel
 from config import BertPretrainedModelPath
 import numpy as np
+import math
 
 class Embedding_layer(nn.Module):
     """embedding layer and provide some method to freeze layer parameters"""
@@ -137,5 +136,100 @@ class SynSetClassfier(nn.Module):
             
             
 
-        
+"""Model Based On Attention """     
 
+def calculate_attention(query:torch.Tensor,key:torch.Tensor,value:torch.Tensor,mask:torch.Tensor):
+    """Calclulate Attention 
+    @param:
+        query: torch.Tensor (Batch_size, max_seq_len, hidden_size)
+        key: torch.Tensor (Batch_size, max_seq_len, hidden_size)
+        value : torch.Tensor(Batch_size, max_seq_len, word_vector_size)
+        mask : torch.Tensor.Boolean(Batch_size, max_seq_len);
+    """
+    hidden_size = query.size(-1)
+    max_seq_size = mask.size(-1)
+    scores = torch.matmul(query, key.transpose(-1,-2)) / math.sqrt(hidden_size)
+    #scores (batch_size, max_seq_len , max_seq_len)
+    mask_ = mask.unsqueeze(-1).expand(-1,-1,max_seq_size)
+    scores = scores.masked_fill(mask_ == 0, -1e9)
+
+    attention_matrix = torch.softmax(scores, dim = -1)
+    # (batch_size, max_seq_len , max_seq_len)
+
+    output = torch.matmul(attention_matrix,value)
+
+    return output
+
+
+class Attention_layer(nn.Module):
+    """Attention Unit"""
+    def __self__(self,kernel_input:int, kernel_output:int):
+        super(self,Attention_layer).__init__()
+        key_kernel = nn.Linear(kernel_input, kernel_output)
+        query_kernel = nn.Linear(kernel_input, kernel_output)
+        value_kernel = nn.Linear(kernel_input, kernel_output)
+        normalize_kernel = nn.Linear(kernel_output,1,bias = False)
+
+    def forward(self,x:torch.Tensor, mask:torch.Tensor):
+        """
+        @params:
+            x: (batch_size, max_seq_len, word_emb_size)
+            mask: (batch_size, max_seq_len)
+        """
+        key = self.key_kernel(x)
+        query = self.query_kernel(x)
+        value = self.value_kernel(x)
+        #batch_size,max_seq_len, hidden_size
+        
+        att_output = calculate_attention(query, key, value, mask)
+        #batch_size, max_seq_len, hidden_size
+        att_output = self.normalize_kernel(att_output)
+        #batch_size, max_seq_len, 1
+        return att_output
+
+class BinarySynClassifierBaseOnAttentionn(nn.Module):
+    """SynSet Classifier Based on Attention"""
+
+    def __init__(self,config:Dict):
+        super(self,BinarySynClassifierBaseOnAttentionn).__init__()
+        config = config
+        self.name = config['name']
+        self.version = config['version']
+        self.embedding = config['embedding']
+        self.attention_unit = config['attention']
+        self.classifier = getFCLayer([self.embedding.dim * 5, *config['classifier_hidden_size']], 1)
+
+    def forward(self,word_set:torch.Tensor,mask:torch.Tensor,waiting_word:torch.Tensor):
+        """
+        @params:
+            word_set: torch.Tensor (batch_size, max_seq_len)
+            waiting_word: torch.Tensor (batch_size)
+            mask: torch.Tesnor (batch_size, max_seq_len)
+        """
+        word_set = self.embedding(word_set)
+        waiting_word = self.embedding(waiting_word)
+        # batch_size max_seq_len word_vec_size, batch_size, word_vec_size
+        
+        word_set_weight= self.attention_unit(word_set,mask);
+        # batch_size, max_seq_len,1
+        word_set_weight = word_set_weight.expand(-1,-1,word_set.size(-1))
+        # batch_size, max_seq_len, word_vec_size
+
+        word_set_vec = torch.sum(word_set_weight * word_set, dim = 1)
+        # batch_size, word_vec_size
+
+        com_feature = self.extract_feature(word_set_vec, waiting_word)
+
+        ans = self.classifier(com_feature)
+        # batch_size , 1
+
+        return torch.sigmoid(ans)
+
+    def extract_feature(self,x:torch.Tensor,y:torch.Tensor):
+        add_feature = x + y
+        divide_feature = x - y
+        multiply_feature = x * y
+        # batch_size, word_vec_size
+        ans = torch.cat([add_feature,divide_feature,multiply_feature,x,y], dim = -1)
+        return ans
+        
